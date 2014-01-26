b7f986f1834c8d821a007c06f4422ba87b2c4254
EdbertChan <edbert.chan@yahoo.com>
2014-01-26 05:41:47 -0800

    Allowed for university id for easier API calls and removed welcome to simple app

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140126100447) do
+ActiveRecord::Schema.define(version: 20140126133650) do
 
   create_table "courses", force: true do |t|
     t.string   "courseName"
@@ -40,9 +40,10 @@ ActiveRecord::Schema.define(version: 20140126100447) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
-    t.boolean  "admin",           default: false
+    t.boolean  "admin",                default: false
     t.string   "avatar"
     t.string   "university"
+    t.string   "universityIdentifier"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
