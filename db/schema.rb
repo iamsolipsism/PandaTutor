8932527e95c33ea82e0296bc9520a2f680a30557
EdbertChan <edbert.chan@yahoo.com>
2014-01-26 02:24:56 -0800

    Fixed bug regarding objects and error messages when editing the profile and adding grades

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140126074711) do
+ActiveRecord::Schema.define(version: 20140126100447) do
 
   create_table "courses", force: true do |t|
     t.string   "courseName"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20140126074711) do
     t.string   "remember_token"
     t.boolean  "admin",           default: false
     t.string   "avatar"
+    t.string   "university"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
