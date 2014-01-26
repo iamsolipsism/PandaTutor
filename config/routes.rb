e826ef5f74a8b5af7cd8ea82d7966dc085532eaa
EdbertChan <edbert.chan@yahoo.com>
2014-01-26 04:20:40 -0800

    Aesthetics and added in a functioning search API. Unsure about desktop product b/c it doesnt look functional.

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,9 @@ end
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-	match '/signup', 	to: 'users#new',						via: 'get'
+
+	match '/signup', 	to: 'users#new',						via:
+ 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
 	
