fa143c20c520c49de1b832234028b966b21af455
liu.titin@gmail.com <liu.titin@gmail.com>
2014-01-25 18:28:36 -0800

    Profile Pic
    Wheeeee

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140125232804) do
+ActiveRecord::Schema.define(version: 20140126011732) do
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
 
   create_table "searches", force: true do |t|
     t.datetime "created_at"
@@ -26,6 +35,7 @@ ActiveRecord::Schema.define(version: 20140125232804) do
     t.string   "password_digest"
     t.string   "remember_token"
     t.boolean  "admin",           default: false
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
