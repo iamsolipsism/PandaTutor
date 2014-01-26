8cd6afaed1f61414d06e51df0be5297c6a6f8c78
Austin Chen <akrolsmir@gmail.com>
2014-01-25 19:49:48 -0800

    Remove rename migration

--- db/migrate/20140126024150_rename_department_column_to_course.rb
+++
@@ -1,9 +0,0 @@
-class RenameDepartmentColumnToCourse < ActiveRecord::Migration
-  def self.up
-    rename_column :kurasus, :department, :course
-  end
-
-  def self.down
-    rename_column :kurasus, :course, :department
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140126024150) do
+ActiveRecord::Schema.define(version: 20140126011732) do
 
   create_table "kurasus", force: true do |t|
     t.string   "course"
