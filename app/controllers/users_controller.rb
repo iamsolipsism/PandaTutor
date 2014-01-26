4cb40039c2e69bc6e3a32638da201a0bba68a03c
liu.titin@gmail.com <liu.titin@gmail.com>
2014-01-26 00:00:32 -0800

    Fixed Flash Text upon Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ respond_to :json, :xml
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to the Panda Tutor!"
       redirect_to @user
     else
       render 'new'
